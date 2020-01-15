69f995f1f544baa561c9702226499fd0a0313857
smuralee <suraj@surajmuraleedharan.com>
2020-01-14 22:10:32 -0600

    Added Typescript examples

---
+++ ts-samples/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function index(person) {
+    return "Hello, " + person;
+}
+var username = "Jane Doe";
+var message = index(username);
+console.log(message);
---
+++ ts-samples/src/index.ts
@@ -0,0 +1,8 @@
+function index(person: string) {
+    return "Hello, " + person;
+}
+
+let username = "Jane Doe";
+
+const message = index(username);
+console.log(message);
