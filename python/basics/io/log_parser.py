0b7ee6bb4e58b13c27efdedf24c2c6a36440bd06
smuralee <suraj@surajmuraleedharan.com>
2020-02-24 11:11:32 +0000

    Edits for gitignore and variable name

--- python/basics/io/log_parser.py
+++ python/basics/io/log_parser.py
@@ -1,12 +1,12 @@
 # Read the file and split into lines
-with open('in/log.txt') as freader:
-    file_contents = freader.read().splitlines()
+with open('in/log.txt') as f_reader:
+    file_contents = f_reader.read().splitlines()
 
 # Remove all the empty lines
 file_contents = filter(None, file_contents)
 
 # Create a new file for writing the clean data
-fwriter = open("out/log.txt", "w")
+f_writer = open("out/log.txt", "w")
 
 # List to store the lines
 data = []
@@ -15,11 +15,11 @@ data = []
 for file_line in file_contents:
     if '#' in file_line:
         # Write to the cleaned data file as comma delimited
-        fwriter.write('{}'.format(",".join(data)))
-        fwriter.write('\n')
+        f_writer.write('{}'.format(",".join(data)))
+        f_writer.write('\n')
         data.clear()
     else:
         data.append(file_line)
 
 # Close the file
-fwriter.close()
+f_writer.close()
