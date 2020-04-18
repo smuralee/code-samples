a604582b26afbffe658cc1191b516398f94ee640
smuralee <suraj@surajmuraleedharan.com>
2020-04-18 10:42:12 +0100

    Matrix transpose

--- src/main/java/com/smuralee/matrix/Solution.java
+++ src/main/java/com/smuralee/matrix/Solution.java
@@ -4,17 +4,18 @@ public class Solution {
 
     /**
      * <p>
-     *     For an array row [1,2,3] - The flip will be 1 to 3, 3 to 1 and 2 is static<br/>
-     *     For and array row [1,2,3,4] - The flip is 1 to 4, 4 -1 and 2 to 3, 3 to 2<br/>
-     *     For and array row [1,2,3,4,5] - The flip is 1 to 5, 5 -1 ; 2 to 4, 4 to 2 and 3 is static<br/>
-     *     <br/>
-     *     Number of flips = row length/2<br/>
-     *     For even number of elements [4], all flip [2 flips]<br/>
-     *     For odd number of elements [5], centre is untouched [2 flips]<br/>
-     *     <br/>
-     *     The loop on the row will be (int i = 1; i <= row.length / 2; i++)<br/>
-     *     Initializing with i=1 since we need the index is, row.length -1<br/>
+     * For an array row [1,2,3] - The flip will be 1 to 3, 3 to 1 and 2 is static<br/>
+     * For and array row [1,2,3,4] - The flip is 1 to 4, 4 -1 and 2 to 3, 3 to 2<br/>
+     * For and array row [1,2,3,4,5] - The flip is 1 to 5, 5 -1 ; 2 to 4, 4 to 2 and 3 is static<br/>
+     * <br/>
+     * Number of flips = row length/2<br/>
+     * For even number of elements [4], all flip [2 flips]<br/>
+     * For odd number of elements [5], centre is untouched [2 flips]<br/>
+     * <br/>
+     * The loop on the row will be (int i = 1; i <= row.length / 2; i++)<br/>
+     * Initializing with i=1 since we need the index is, row.length -1<br/>
      * </p>
+     *
      * @param mat - matrix for flipping
      * @return the matrix after horizontal flip
      */
@@ -26,6 +27,44 @@ public class Solution {
                 tempVar = row[row.length - i];
                 row[row.length - i] = row[i - 1];
                 row[i - 1] = tempVar;
+            }
+        }
+        return mat;
+    }
+
+    /**
+     * <p>
+     * Given matrix <br/>
+     * {1, 2, 3, 11},<br/>
+     * {4, 5, 6, 14},<br/>
+     * {7, 8, 9, 17},<br/>
+     * {10, 20, 30, 40}
+     * <br/>
+     * </p>
+     * <br/>
+     * <p>
+     * Matrix positions <br/>
+     * {0-0, 0-1, 0-2, 0-3},<br/>
+     * {1-0, 1-1, 1-2, 1-3},<br/>
+     * {2-0, 2-1, 2-2, 2-3},<br/>
+     * {3-0, 3-1, 3-2, 3-3}<br/>
+     * <p>
+     * <br/>
+     * row is 'i' and column is 'j'<br/>
+     * The transpose happens at the vertex of i=j, every time j = i+1<br/>
+     * The elements to transpose are to the right and bottom of i=j <br/>
+     * So we flip [i][j] = [j][i]<br/>
+     *
+     * @param mat - Given matrix to transpose
+     * @return - Transposed matrix
+     */
+    public int[][] transpose_matrix(int[][] mat) {
+        int tempVar;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = i + 1; j < mat.length; j++) {
+                tempVar = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = tempVar;
             }
         }
         return mat;
--- src/test/java/com/smuralee/matrix/SolutionTest.java
+++ src/test/java/com/smuralee/matrix/SolutionTest.java
@@ -17,14 +17,45 @@ class SolutionTest {
     }
 
     @Test
-    @DisplayName("GIVEN an array, THEN flip the elements horizontally")
+    @DisplayName("GIVEN a matrix, THEN flip the elements horizontally")
     void flip_matrix_horizontally() {
 
-        int[][] given = new int[][]{{1, 2, 3, 11}, {4, 5, 6, 14}, {7, 8, 9, 17}, {100, 200, 300, 400}};
-        int[][] expected = new int[][]{{11, 3, 2, 1}, {14, 6, 5, 4}, {17, 9, 8, 7}, {400, 300, 200, 100}};
+        int[][] given = new int[][]{
+                {1, 2, 3, 11},
+                {4, 5, 6, 14},
+                {7, 8, 9, 17},
+                {10, 20, 30, 40}
+        };
+        int[][] expected = new int[][]{
+                {11, 3, 2, 1},
+                {14, 6, 5, 4},
+                {17, 9, 8, 7},
+                {40, 30, 20, 10}
+        };
 
         int[][] response = solution.flip_matrix_horizontally(given);
         assertArrayEquals(expected, response);
 
+    }
+
+    @Test
+    @DisplayName("GIVEN a matrix, THEN transpose the matrix")
+    void transpose_matrix() {
+
+        int[][] given = new int[][]{
+                {1, 2, 3, 11},
+                {4, 5, 6, 14},
+                {7, 8, 9, 17},
+                {10, 20, 30, 40}
+        };
+        int[][] expected = new int[][]{
+                {1, 4, 7, 10},
+                {2, 5, 8, 20},
+                {3, 6, 9, 30},
+                {11, 14, 17, 40}
+        };
+
+        int[][] response = solution.transpose_matrix(given);
+        assertArrayEquals(expected, response);
     }
 }
